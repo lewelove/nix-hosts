0c8d37bcfcdbbf6099273c0a56e9f6f86c3d9b5e
lewelove <lewelove@proton.me>
2025-12-23 22:26:35 +0300

    2025-12-23 19:26 UTC

--- lab/services/tailscale.nix
+++ lab/services/tailscale.nix
@@ -13,6 +13,14 @@
   networking.firewall = {
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
+    extraCommands = ''
+      ip rule add to 100.64.0.0/10 lookup 52 priority 2500 || true
+      iptables -t mangle -A FORWARD -o active -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu || true
+    '';
+    extraStopCommands = ''
+      ip rule del to 100.64.0.0/10 lookup 52 || true
+      iptables -t mangle -D FORWARD -o active -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu || true
+    '';
   };
 
   boot.kernel.sysctl = {
