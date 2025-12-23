cde46c2e866575f279d68fab2e3f1b98ec57f4c3
lewelove <lewelove@proton.me>
2025-12-23 18:23:10 +0300

    2025-12-23 15:23 UTC

--- common/scripts/ns.nix
+++ common/scripts/ns.nix
@@ -14,6 +14,7 @@ let
       # 2. Stow Common Workflow
       if [ -d "$REPO_DIR/common/tilde" ]; then
           echo ":: Stowing Common Workflow..."
+          echo
           cd "$REPO_DIR/common"
           stow --adopt -t "$HOME" tilde --verbose=1
       fi
@@ -21,25 +22,28 @@ let
       # 3. Stow Host Specifics
       if [ -d "${hostPath}/tilde" ]; then
           echo ":: Stowing Host Specifics..."
+          echo
           cd "${hostPath}"
           stow --adopt -t "$HOME" tilde --verbose=1
       fi
 
       # 4. Sync Git Repository
-      echo -e "\n:: Syncing Git..."
+      echo ":: Syncing Git..."
+      echo
       cd "$REPO_DIR"
       git add .
       git commit -m "$MSG"
       git push
+      echo
           
       # 5. Refresh LLM Context
       if command -v repomix &> /dev/null; then
           echo "Repomixing $REPO_DIR..."
           repomix --quiet
           echo "Repomixing [home] host..."
-          repomix --include "common/**,home/**"
+          repomix --quiet --include "common/**,home/**"
           echo "Repomixing [lab] host..."
-          repomix --include "common/**,lab/**"
+          repomix --quiet --include "common/**,lab/**"
       fi
     '';
   };
