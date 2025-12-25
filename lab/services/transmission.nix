a6d1b9cae51b6d57a7d16b046d915982ece7b5b3
lewelove <lewelove@proton.me>
2025-12-25 20:20:58 +0300

    2025-12-25 17:20 UTC

--- lab/services/transmission.nix
+++ lab/services/transmission.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 {
-
   networking.firewall.allowedTCPPorts = [ 9091 54321 ];
 
   systemd.tmpfiles.rules = [
@@ -15,31 +14,25 @@
     enable = true;
     group = "jellyfin"; 
     settings = {
-
       umask = 2;
-
       download-dir = "/mnt/drives/hdd1000/media/torrents";
       incomplete-dir-enabled = false;
-
       cache-size-mb = 1024;
       peer-limit-global = 2000;
       peer-limit-per-torrent = 500;
       encryption = 1;
-
       peer-port = 54321;
       port-forwarding-enabled = false;
-      
       rpc-bind-address = "0.0.0.0";
       rpc-port = 9091;
       rpc-whitelist-enabled = true;
       rpc-whitelist = "127.0.0.1,192.168.*.*,100.*.*.*,10.*.*.*";
       rpc-host-whitelist-enabled = false;
-      
     };
   };
 
   systemd.services.transmission-bypass = {
-    description = "Routing rules to bypass VPN for Transmission user";
+    description = "Routing rules to bypass VPN for Transmission user (using Table 101)";
     after = [ "network.target" "awg-vpn.service" ];
     wantedBy = [ "multi-user.target" ];
     
@@ -50,11 +43,8 @@
 
     script = ''
       ${pkgs.iproute2}/bin/ip route add default via 192.168.1.1 dev enp2s0 table 101 || true
-      
-      ${pkgs.iproute2}/bin/ip rule add fwmark 1 lookup 101 priority 10 || true
-
+      ${pkgs.iproute2}/bin/ip rule add fwmark 1 lookup 101 priority 1000 || true
       ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT -m owner --uid-owner transmission -j MARK --set-mark 1
-      
       ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o enp2s0 -m mark --mark 1 -j MASQUERADE
     '';
 
@@ -62,7 +52,7 @@
       ${pkgs.iproute2}/bin/ip rule del fwmark 1 lookup 101 || true
       ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT -m owner --uid-owner transmission -j MARK --set-mark 1 || true
       ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o enp2s0 -m mark --mark 1 -j MASQUERADE || true
+      ${pkgs.iproute2}/bin/ip route flush table 101 || true
     '';
   };
-
 }
