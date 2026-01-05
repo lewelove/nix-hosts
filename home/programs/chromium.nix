68a65017e4aa2d64e14f7a4462d67b2d79fea236
lewelove <lewelove@proton.me>
2026-01-05 13:31:57 +0300

    2026-01-05 10:31 UTC

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -1,34 +1,62 @@
-{ pkgs, username, ... }:
+{ pkgs, lib, username, ... }:
 
 let
 
-  chromiumWebStore = pkgs.stdenv.mkDerivation rec {
-    pname = "chromium-web-store";
-    version = "1.5.5.2";
-    src = pkgs.fetchFromGitHub {
-      owner = "NeverDecaf";
-      repo = "chromium-web-store";
-      rev = "v${version}";
-      hash = "sha256-Rr0KVs6Ztqz04CpQSDThn/hi6VZdVZsztPSALUY/fnE=";
+  fetchExtension = { id, version, hash }: {
+    inherit id version;
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${lib.versions.major pkgs.ungoogled-chromium.version}&x=id%3D${id}%26installsource%3Dondemand%26uc";
+      name = "${id}.crx";
+      sha256 = hash;
     };
-    installPhase = "mkdir -p $out; cp -r src/* $out/";
   };
 
-  ublockOrigin = pkgs.stdenv.mkDerivation rec {
-    pname = "ublock-origin";
-    version = "1.68.0";
-    src = pkgs.fetchurl {
-      url = "https://github.com/gorhill/uBlock/releases/download/${version}/uBlock0_${version}.chromium.zip";
-      hash = "sha256-lpb7CN2QbnH10gD+EH7YUptQMri1kLTUz0nTvxMgEOM=";
+  chromium-web-store = rec {
+    id = "";
+    drv = pkgs.stdenv.mkDerivation rec {
+      pname = "chromium-web-store";
+      version = "1.5.5.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "NeverDecaf";
+        repo = "chromium-web-store";
+        rev = "v${version}";
+        hash = "sha256-Rr0KVs6Ztqz04CpQSDThn/hi6VZdVZsztPSALUY/fnE=";
+      };
+      installPhase = "mkdir -p $out; cp -r src/* $out/";
     };
-    nativeBuildInputs = [ pkgs.unzip ];
-    unpackPhase = "unzip $src -d .";
-    installPhase = "mkdir -p $out; cp -r uBlock0.chromium/* $out/";
   };
 
-  extensions = "${chromiumWebStore},${ublockOrigin}";
+  ublock-origin = rec {
+    id = "mdcpogggagpjibjhpohkefbfgfaepcik";
+    drv = pkgs.stdenv.mkDerivation rec {
+      pname = "ublock-origin";
+      version = "1.68.0";
+      src = pkgs.fetchurl {
+        url = "https://github.com/gorhill/uBlock/releases/download/${version}/uBlock0_${version}.chromium.zip";
+        hash = "sha256-lpb7CN2QbnH10gD+EH7YUptQMri1kLTUz0nTvxMgEOM=";
+      };
+      nativeBuildInputs = [ pkgs.unzip ];
+      unpackPhase = "unzip $src -d .";
+      installPhase = "mkdir -p $out; cp -r uBlock0.chromium/* $out/";
+    };
+  };
 
-  ublockId = "mdcpogggagpjibjhpohkefbfgfaepcik";
+  untrap = fetchExtension {
+    id = "enboaomnljigfhfjfoalacienlhjlfil";
+    version = "9.3.7";
+    hash = "sha256-606Xbwjq9c53i/cMsIwOfnDBp0Mf8Ogds6ppEw5lEy0=";
+  };
+
+  sideloaded = [
+    chromium-web-store
+    ublock-origin
+  ];
+
+  webstore = [
+    untrap
+  ];
+
+  extensions = sideloaded ++ webstore;
 
 in
 
@@ -37,22 +65,21 @@ in
   programs.chromium = {
     enable = true;
     extraOpts = {
-      "ExtensionSettings" = {
-        "${ublockId}" = {
-          "installation_mode" = "allowed";
-        };
-      };
-      "ExtensionInstallAllowlist" = [ ublockId ];
-      "DeveloperToolsAvailability" = 1;
+      "ExtensionSettings" = builtins.listToAttrs (map (ext: {
+        name = ext.id;
+        value = { installation_mode = "allowed"; };
+      }) extensions);
     };
   };
-
   home-manager.users.${username} = {
     programs.chromium = {
       enable = true;
       package = pkgs.ungoogled-chromium;
+
+      extensions = webstore;
+
       commandLineArgs = [
-        "--load-extension=${extensions}"
+        "--load-extension=${lib.concatStringsSep "," (map (ext: "${ext.drv}") sideloaded)}"
         "--extension-mime-request-handling=always-prompt-for-install"
         "--no-default-browser-check"
         "--restore-last-session"
