b340cf295f028cbad4053157ab99c15f65645d3b
lewelove <lewelove@proton.me>
2026-01-06 12:00:26 +0300

    2026-01-06 09:00 UTC

--- common/scripts/ns.nix
+++ common/scripts/ns.nix
@@ -8,14 +8,18 @@ let
 
 ################################################################
 
-wb() { gum style --foreground 7 --bold "$*"; }
 rb() { gum style --foreground 1 --bold "$*"; }
 gb() { gum style --foreground 2 --bold "$*"; }
+yb() { gum style --foreground 3 --bold "$*"; }
 bb() { gum style --foreground 4 --bold "$*"; }
-w() { gum style --foreground 7 "$*"; }
+mb() { gum style --foreground 5 --bold "$*"; }
+wb() { gum style --foreground 7 --bold "$*"; }
 r() { gum style --foreground 1 "$*"; }
 g() { gum style --foreground 2 "$*"; }
+y() { gum style --foreground 3 "$*"; }
 b() { gum style --foreground 4 "$*"; }
+m() { gum style --foreground 5 "$*"; }
+w() { gum style --foreground 7 "$*"; }
 
 # tilde-stow
 
@@ -24,26 +28,26 @@ MSG="''${1:-$(date -u +'%Y-%m-%d %H:%M UTC')}"
 cd "${repoPath}"
 
 echo
-gum join --horizontal ":: Repomixing " "$(g "${repoPath}...")"
+gum join --horizontal "$(g ">")" " Repomixing " "$(g "${repoPath}...")"
 repomix --quiet --include "common/**,home/**"
 repomix --quiet --include "common/**,lab/**"
-repomix --quiet --include "common/**,note/**"
+# repomix --quiet --include "common/**,note/**"
 echo
 
-gum join --horizontal ":: Adding with message " "$(g "$MSG")" "..."
+gum join --horizontal "$(g ">")" " Adding with message " "$(p "$MSG")" "..."
 echo
 git add .
 git commit -m "$MSG"
 echo
 
 if git remote | grep -q "^lab$"; then
-    gum join --horizontal ":: Syncing to " "$(b "lab")" " via LAN..."
+    gum join --horizontal "$(g ">")" " Syncing to " "$(b "lab")" " via LAN..."
     echo
     git push lab main
     echo
 fi
 
-gum join --horizontal ":: Syncing to " "$(g "origin")" "..."
+gum join --horizontal "$(g ">")" " Syncing to " "$(g "origin")" "..."
 echo
 git push -u origin main
     
