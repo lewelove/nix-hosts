d66ea4cbeb178f9ee8dd8582cc6e85379f47d5c2
lewelove <lewelove@proton.me>
2026-02-12 16:14:58 +0300

    2026-02-12 13:14 UTC

--- lab/services/openclaw.nix
+++ lab/services/openclaw.nix
@@ -1,8 +1,9 @@
 { config, pkgs, inputs, username, ... }:
 
 {
-  # Add the OpenClaw Home Manager module to the user configuration
+
   home-manager.users.${username} = {
+
     imports = [ inputs.openclaw.homeManagerModules.openclaw ];
 
     programs.openclaw = {
@@ -10,27 +11,9 @@
 
       package = inputs.openclaw.packages.${pkgs.system}.openclaw;
       
-      # Points to the directory where you keep your bot's identity files
       documents = ../tilde/openclaw-docs;
 
-      config = {
-        gateway = {
-          mode = "local";
-          auth.token = "change-this-to-a-secure-random-string"; 
-        };
 
-        channels.telegram = {
-          # Path to a file containing your bot token (create this manually on the server)
-          tokenFile = "/home/${username}/.secrets/telegram-token";
-          # Your Telegram User ID (get it from @userinfobot)
-          allowFrom = [ 7976595060 ]; 
-          groups = {
-            "*" = { requireMention = true; };
-          };
-        };
-      };
-
-      # First-party plugins to give the bot "senses"
       bundledPlugins = {
         summarize.enable = true;   # Summarize URLs
       };
@@ -38,14 +21,28 @@
       instances.default = {
         enable = true;
         systemd.enable = true;
+        config = {
+          gateway = {
+            mode = "local";
+            auth.token = "change-this-to-a-secure-random-string"; 
+          };
+
+          channels.telegram = {
+            tokenFile = "/home/${username}/.secrets/telegram-token";
+            allowFrom = [ 7976595060 ]; 
+            groups = {
+              "*" = { requireMention = true; };
+            };
+          };
+        };
       };
     };
 
-    # This injects the environment variables into the bot's service
     systemd.user.services.openclaw-gateway = {
       Service = {
         EnvironmentFile = [ "/home/${username}/.secrets/openclaw.env" ];
       };
     };
   };
+
 }
