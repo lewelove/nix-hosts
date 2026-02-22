62a30c2657e2326ed347e596e2ac21ef833e8b4a
lewelove <lewelove@proton.me>
2026-02-22 13:55:28 +0300

    2026-02-22 10:55 UTC

--- dotfiles/.scripts/wrap-paste.sh
+++ dotfiles/.scripts/wrap-paste.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 CONTENT=$(wl-paste)
-
 if [ -z "$CONTENT" ]; then
     exit 0
 fi
 
-WRAPPED=$(printf '\n```\n%s\n```\n' "$CONTENT")
+wtype -k Return
+wtype '```'
+wtype -k Return
 
-echo -n "$WRAPPED" | wl-copy
-
-sleep 0.05
 wtype -M ctrl v -m ctrl
+
+sleep 0.1
+
+wtype -k Return
+wtype '```'
+wtype -k Return
