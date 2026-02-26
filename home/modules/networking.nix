947efeed8d0c32f174e17737376455be7c74ae8b
lewelove <lewelove@proton.me>
2026-02-26 22:51:36 +0300

    2026-02-26 19:51 UTC

--- home/modules/networking.nix
+++ home/modules/networking.nix
@@ -1,13 +1,13 @@
 { config, pkgs, username, hostname, ... }:
 
 {
-
   networking = {
     hostName = hostname;
     networkmanager.enable = true;
-    firewall.checkReversePath = "loose";
-    interfaces.enp3s0.wakeOnLan.enable = true;
+    
+    interfaces.enp3s0.wakeOnLan.enable = true; 
 
+    firewall.checkReversePath = "loose";
     firewall.allowedTCPPorts = [ 80 8080 6600 ];
     firewall.allowedUDPPorts = [ 9 ];
 
@@ -16,9 +16,9 @@
       192.168.1.100 qbittorrent.lab
       192.168.1.100 jellyfin.lab
     '';
-
-    environment.systemPackages = [ pkgs.ethtool ];
-
   };
 
+  environment.systemPackages = [ 
+    pkgs.ethtool 
+  ];
 }
