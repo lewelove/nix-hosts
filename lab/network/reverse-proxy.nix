46e1ffafd2e5fc4fb31b975da5ee3e13e83d4e3f
lewelove <lewelove@proton.me>
2026-03-10 16:11:32 +0300

    2026-03-10 13:11 UTC

--- lab/network/reverse-proxy.nix
+++ lab/network/reverse-proxy.nix
@@ -7,19 +7,19 @@
     environmentFile = "/etc/duckdns.env";
 
     virtualHosts = {
-      "{$DUCKDNS_DOMAIN}.duckdns.org" = {
+      "{$DUCKDNS_DOMAIN}" = {
         extraConfig = ''
           reverse_proxy localhost:8096
         '';
       };
       
-      "torrents.{$DUCKDNS_DOMAIN}.duckdns.org" = {
+      "torrents.{$DUCKDNS_DOMAIN}" = {
         extraConfig = ''
           reverse_proxy localhost:8080
         '';
       };
 
-      "sync.{$DUCKDNS_DOMAIN}.duckdns.org" = {
+      "sync.{$DUCKDNS_DOMAIN}" = {
         extraConfig = ''
           reverse_proxy localhost:8384
         '';
