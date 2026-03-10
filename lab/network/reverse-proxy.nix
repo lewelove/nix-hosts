6bc77c28d15c3a079e299e9bb82a422266774cd2
lewelove <lewelove@proton.me>
2026-03-10 16:39:40 +0300

    2026-03-10 13:39 UTC

--- lab/network/reverse-proxy.nix
+++ lab/network/reverse-proxy.nix
@@ -12,7 +12,12 @@
           basicauth * {
             lewelove $2a$14$QCg4lYo7UFzsX7HwZYIdn.D/F05QFh4lp121dcpRhRFdDb14TP3Ju
           }
-          reverse_proxy localhost:8096
+          reverse_proxy localhost:8096 {
+            header_up Host {host}
+            header_up X-Real-IP {remote_host}
+            header_up X-Forwarded-For {remote_host}
+            header_up X-Forwarded-Proto {scheme}
+          }
         '';
       };
       
