fbe9a0d5fff7d8323d8c736710f21ec6ea303d0d
lewelove <lewelove@proton.me>
2026-03-10 16:51:36 +0300

    2026-03-10 13:51 UTC

--- lab/network/auth.nix
+++ lab/network/auth.nix
@@ -3,18 +3,26 @@
 {
   services.authelia.instances.main = {
     enable = true;
-    secrets.storageEncryptionKeyFile = "/etc/authelia-storage-key";
-    secrets.jwtSecretFile = "/etc/authelia-jwt-secret";
+    secrets = {
+      storageEncryptionKeyFile = "/etc/authelia-storage-key";
+      jwtSecretFile = "/etc/authelia-jwt-secret";
+      sessionSecretFile = "/etc/authelia-session-secret";
+    };
     
     settings = {
       theme = "dark";
-      server.address = "tcp://127.0.0.1:9091";
+      server = {
+        host = "127.0.0.1";
+        port = 9091;
+        path = "/authelia";
+      };
+      log.level = "debug";
       authentication_backend.file.path = "/var/lib/authelia-main/users.yml";
       storage.local.path = "/var/lib/authelia-main/db.sqlite3";
       
       session = {
         name = "authelia_session";
-        domain = "duckdns.org";
+        domain = "lewelaboratory.duckdns.org";
         expiration = "1h";
         inactivity = "30m";
         remember_me = "30d";
--- lab/network/reverse-proxy.nix
+++ lab/network/reverse-proxy.nix
@@ -8,18 +8,17 @@
     virtualHosts = {
       "{$DUCKDNS_DOMAIN}" = {
         extraConfig = ''
-          forward_auth localhost:9091 {
-            uri /api/verify?rd=https://{$DUCKDNS_DOMAIN}/authelia/
-            copy_headers Remote-User Remote-Groups Remote-Name Remote-Email
+          handle /authelia* {
+            reverse_proxy localhost:9091
           }
 
-          reverse_proxy localhost:8096
-        '';
-      };
-
-      "{$DUCKDNS_DOMAIN}/authelia/*" = {
-        extraConfig = ''
-          reverse_proxy localhost:9091
+          handle {
+            forward_auth localhost:9091 {
+              uri /authelia/api/verify?rd=https://{$DUCKDNS_DOMAIN}/authelia/
+              copy_headers Remote-User Remote-Groups Remote-Name Remote-Email
+            }
+            reverse_proxy localhost:8096
+          }
         '';
       };
     };
