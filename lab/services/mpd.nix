8d0f1ddd0123faa1e98baaeda9aa7929a0392eb6
lewelove <lewelove@proton.me>
2026-03-14 00:26:30 +0300

    2026-03-13 21:26 UTC

--- lab/services/mpd.nix
+++ lab/services/mpd.nix
@@ -4,15 +4,16 @@
   services.mpd = {
     enable = true;
     musicDirectory = "/mnt/1000xlab/backup-everything/FB2K/Library Historyfied!";
-    extraConfig = ''
-      audio_output {
-        type "null"
-        name "My Lossless Device"
-      }
+    
+    settings = {
+      bind_to_address = "127.0.0.1";
+      port = "6600";
       
-      bind_to_address "127.0.0.1"
-      port "6600"
-    '';
+      audio_output = {
+        type = "null";
+        name = "My Lossless Device";
+      };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 6600 ];
