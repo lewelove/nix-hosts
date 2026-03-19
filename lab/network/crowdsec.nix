d245876e85901cd455bba87d9153422e2796d02a
lewelove <lewelove@proton.me>
2026-03-19 14:20:53 +0300

    2026-03-19 11:20 UTC

--- lab/network/crowdsec.nix
+++ lab/network/crowdsec.nix
@@ -3,23 +3,24 @@
 {
   services.crowdsec = {
     enable = true;
-    allowStreamMode = true;
     
-    acquisitions = [
-      {
-        source = "journalctl";
-        journalctl_filter = [ "_SYSTEMD_UNIT=sshd.service" ];
-        labels.type = "syslog";
-      }
-      {
-        source = "journalctl";
-        journalctl_filter = [ "_SYSTEMD_UNIT=caddy.service" ];
-        labels.type = "caddy";
-      }
-    ];
+    settings = {
+    };
   };
 
-  services.crowdsec.bouncer.firewall = {
-    enable = true;
-  };
+  environment.etc."crowdsec/acquisitions.yaml".text = ''
+    source: journalctl
+    journalctl_filter:
+     - _SYSTEMD_UNIT=sshd.service
+    labels:
+      type: syslog
+    ---
+    source: journalctl
+    journalctl_filter:
+     - _SYSTEMD_UNIT=caddy.service
+    labels:
+      type: caddy
+  '';
+
+  services.crowdsec.bouncer.firewall.enable = true;
 }
