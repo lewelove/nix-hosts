50236b67b6a1bb971c634833330d2ce6c858e288
lewelove <lewelove@proton.me>
2026-04-01 00:52:54 +0300

    2026-03-31 21:52 UTC

--- lab/services/syncthing.nix
+++ lab/services/syncthing.nix
@@ -7,7 +7,7 @@
   services.syncthing = {
     enable = true;
     user = username;
-    dataDir = "/home/${username}";
+    # configDir = "/home/${username}/.config/syncthing";
     
     guiAddress = "0.0.0.0:8384";
 
