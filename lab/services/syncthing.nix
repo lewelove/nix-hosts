bfef9fd3300b9f7b34c84849e87642282846d31f
lewelove <lewelove@proton.me>
2026-04-01 00:47:34 +0300

    2026-03-31 21:47 UTC

--- lab/services/syncthing.nix
+++ lab/services/syncthing.nix
@@ -1,28 +1,19 @@
 { config, pkgs, username, ... }:
 
-let
-  name = "Syncthing";
-  icon = "syncthing";
-  domain = "syncthing.lab";
-  port = 8384;
-in
 {
   networking.firewall.allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 
-  # services.nginx.virtualHosts."${domain}" = {
-  #   locations."/" = {
-  #     proxyPass = "http://127.0.0.1:${toString port}";
-  #     proxyWebsockets = true;
-  #   };
-  # };
+  services.syncthing = {
+    enable = true;
+    user = username;
+    dataDir = "/home/${username}";
+    configDir = "/home/${username}/.config/syncthing";
+    
+    guiAddress = "0.0.0.0:8384";
 
-  home-manager.users.${username} = {
-    services.syncthing = {
-      enable = true;
-      extraOptions = [
-        "--gui-address=0.0.0.0:8384"
-      ];
+    overrideDevices = false;
+    overrideFolders = false;
     };
   };
 }
